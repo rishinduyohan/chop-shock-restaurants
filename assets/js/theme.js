f448d088a58593f558b46629e9334d6d2a7eb71d
rishinduyohan <rishinduyohan@gmail.com>
2025-08-20 00:30:21 +0530

    Add theme to js file and link it

---
+++ assets/js/theme.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#F3F3F3',
+                secondary: '#312F2F',
+                accent: '#FE5E54',
+                bisque: '#75B8EE',
+                green: '#046E1B',
+                gray: '#D9D9D9',
+                orange: '#F79F1A'
+            },
+            fontFamily: {
+                display: ['Poppins', 'ui-sans-serif', 'system-ui'],
+                body: ['Inter', 'ui-sans-serif', 'system-ui']
+            }
+        }
+    }
+}
