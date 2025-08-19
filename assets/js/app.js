060188a554b909557c4efb3b163d097f1c1c597f
rishinduyohan <rishinduyohan@gmail.com>
2025-08-19 22:32:11 +0530

    Update nav items colour

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
 const btn = document.getElementById('menuBtn');
 const menu = document.getElementById('mobileMenu');
 btn?.addEventListener('click', () => menu.classList.toggle('hidden'));
-
-document.getElementById('year').textContent = new Date().getFullYear();
