4425ff9edc271b8882c1274825ed8f984f98390e
rishinduyohan <rishinduyohan@gmail.com>
2025-08-21 23:00:50 +0530

    link search bar to API search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 //console.log("Hello");
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=milk")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error("Error : ",error));
+
+let  searchItem = document.getElementById("default-search");
+searchItem.addEventListener("keypress", e=>{
+    if(e.key === "Enter"){
+        e.preventDefault();
+        let item = searchItem.value;
+        search(item);
+    }
+});
+search =(item)=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error : ",error));
+}
